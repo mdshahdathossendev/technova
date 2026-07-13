7caa171142065894b77485a59424382b5e0eaa2c
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-07-13 20:40:23 +0600

    add bur uri

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://technova-pied-kappa.vercel.app"
 });
 export const { signIn, signUp, useSession } = createAuthClient()
